3c7d4e9473d521b39dc1b9eaf02912d53ca1a1f8
Peter Carroll <petelc1966@hotmail.com>
2024-09-15 06:57:04 -0400

    Completed DB Structure Script

--- sql-scripts/WMS4SQLite.sql
+++ sql-scripts/WMS4SQLite.sql
@@ -24,6 +24,12 @@ CREATE TABLE "Employees" (
     "ReportsTo" GUID NULL,
     "Role" GUID NULL,
     "Notes" ntext NULL,
+    CONSTRAINT "FK_Employees_Roles" FOREIGN KEY
+    (
+        "Role"
+    ) REFERENCES "Roles" (
+        "RoleId"
+    )
 );
 CREATE INDEX "LastName" ON "Employees"("LastName");
 
@@ -114,5 +120,183 @@ CREATE TABLE "Requests" (
 CREATE INDEX "EmployeeId" ON "Requests"("EmployeeId");
 CREATE INDEX "EmployeesRequests" ON "Requests"("EmployeeId");
 
+CREATE TABLE "Projects" (
+    "ProjectId" GUID PRIMARY KEY,
+    "StatusId" GUID NULL,
+    "ApprovalStatusId" GUID NULL,
+    "Requestor" GUID NULL,
+    "Request" GUID NUll,
+    "CategoryId" GUID NULL,
+    "PriorityId" GUID NULL,
+    "Work" GUID NULL,
+    "ProjectName" NVARCHAR (50) NOT NULL,
+    "Description" NTEXT NULL, 
+    CONSTRAINT "FK_Projects_Status" FOREIGN KEY
+    (
+        "StatusId"
+    ) REFERENCES "Statuses" (
+        "StatusId"
+    ),
+    CONSTRAINT "FK_Projects_ApprovalStatuses" FOREIGN KEY
+    (
+        "ApprovalStatusId"
+    ) REFERENCES "ApprovalStatuses" (
+        "ApprovalStatusId"
+    ),
+    CONSTRAINT "FK_Projects_Priorities" FOREIGN KEY
+    (
+        "PriorityId"
+    ) REFERENCES "Priorities" (
+        "PriorityId"
+    ),
+    CONSTRAINT "FK_Projects_Employees" FOREIGN KEY
+    (
+        "Requestor"
+    ) REFERENCES "Employees" (
+        "EmployeeId"
+    ),
+    CONSTRAINT "FK_Projects_Requests" FOREIGN KEY
+    (
+        "Request"
+    ) REFERENCES "Requests" (
+        "RequestId"
+    ),
+    CONSTRAINT "FK_Projects_Categories" FOREIGN KEY
+    (
+        "CategoryId"
+    ) REFERENCES "Categories" (
+        "CategoryId"
+    ),
+    CONSTRAINT "FK_Projects_Works" FOREIGN KEY
+    (
+        "Work"
+    ) REFERENCES "Works" (
+        "WorkId"
+    )
+);
+CREATE INDEX "CategoriesProjects" ON "Projects"("CategoryId");
+CREATE INDEX "CategoryId" ON "Projects"("CategoryId");
+CREATE INDEX "EmployeesProjects" ON "Projects"("Requestor");
+CREATE INDEX "RequestsProjects" ON "Projects"("Request");
+CREATE INDEX "WorksProjects" ON "Projects"("Work");
 
+CREATE TABLE "Changes" (
+    "ChangeId" GUID PRIMARY KEY,
+    "PriorityId" GUID NULL,
+    "StatusId" GUID NULL,
+    "CategoryId" GUID NULL,
+    "Request" GUID NULL,
+    "Requestor" GUID NULL,
+    "ApprovalStatusId" GUID NULL,
+    "Work" GUID NULL,
+    "ChangeName" NVARCHAR (50) NOT NULL,
+    "Description" NTEXT NULL,
+    CONSTRAINT "FK_Changes_Status" FOREIGN KEY
+    (
+        "StatusId"
+    ) REFERENCES "Statuses" (
+        "StatusId"
+    ),
+    CONSTRAINT "FK_Changes_ApprovalStatuses" FOREIGN KEY
+    (
+        "ApprovalStatusId"
+    ) REFERENCES "ApprovalStatuses" (
+        "ApprovalStatusId"
+    ),
+    CONSTRAINT "FK_Changes_Priorities" FOREIGN KEY
+    (
+        "PriorityId"
+    ) REFERENCES "Priorities" (
+        "PriorityId"
+    ),
+    CONSTRAINT "FK_Changes_Requests" FOREIGN KEY
+    (
+        "Request"
+    ) REFERENCES "Requests" (
+        "RequestId"
+    ),
+    CONSTRAINT "FK_Changes_Categories" FOREIGN KEY
+    (
+        "CategoryId"
+    ) REFERENCES "Categories" (
+        "CategoryId"
+    ),
+    CONSTRAINT "FK_Changes_Employees" FOREIGN KEY
+    (
+        "Requestor"
+    ) REFERENCES "Employees" (
+        "EmployeeId"
+    ),
+    CONSTRAINT "FK_Changes_Works" FOREIGN KEY
+    (
+        "Work"
+    ) REFERENCES "Works" (
+        "WorkId"
+    )
+);
+CREATE INDEX "CategoriesChanges" ON "Changes"("CategoryId");
+CREATE INDEX "CategoryId" ON "Changes"("CategoryId");
+CREATE INDEX "EmployeesChanges" ON "Changes"("Requestor");
+CREATE INDEX "RequestsChanges" ON "Changes"("Request");
+CREATE INDEX "WorksChanges" ON "Changes"("Work");
 
+CREATE TABLE "Works" (
+    "WorkId" GUID PRIMARY KEY
+    "Project" GUID NULL,
+    "Change" GUID NULL,
+    "WorkItem" GUID NULL,
+    "Name" NVARCHAR (50),
+    CONSTRAINT "FK_Works_Projects" FOREIGN KEY
+    (
+        "Project"
+    ) REFERENCES "Projects" (
+        "ProjectId"
+    ),
+    CONSTRAINT "FK_Works_Changes" FOREIGN KEY
+    (
+        "Change"
+    ) REFERENCES "Changes" (
+        "ChangeId"
+    ),
+    CONSTRAINT "FK_WorksWorkItems" FOREIGN KEY
+    (
+        "WorkItem"
+    ) REFERENCES "WorkItems" (
+        "WorkItemId"
+    )
+);
+CREATE INDEX "WorksProjects" ON "Works"("Project");
+CREATE INDEX "WorksChanges" ON "Works"("Change");
+CREATE INDEX "WorksWorkItems" ON "Works"("WorkItems");
+
+CREATE TABLE "WorkItems" (
+    "WorkItemId" GUID PRIMARY KEY,
+    "WorkId" GUID NULL,
+    "PriorityId" GUID NULL,
+    "Assignee" GUID NULL,
+    "CardIDNum" NVARCHAR (10) NULL,
+    "Header" NVARCHAR (25) NULL,
+    "Title" NVARCHAR (25) NULL,
+    "Description" NTEXT NULL,
+    "StartDate" DATETIME2 NULL,
+    "EndDatae" DATETIME2 NULL,
+    CONSTRAINT "FK_WorkItems_Priorities" FOREIGN KEY
+    (
+        "PriorityId"
+    ) REFERENCES "Priorities" (
+        "PriorityId"
+    ),
+    CONSTRAINT "FK_WorkItems_Employee" FOREIGN KEY
+    (
+        "Assignee"
+    ) REFERENCES "Employees" (
+        "EmployeeId"
+    ),
+    CONSTRAINT "FK_WorkItems_ Works" FOREIGN KEY
+    (
+        "WorkId"
+    ) REFERENCES "Works" (
+        "WorkId"
+    )
+);
+CREATE INDEX "WorkItemsWork" ON "WorkItems"("WorkId");
