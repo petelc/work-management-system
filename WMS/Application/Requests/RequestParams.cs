2b03a4a0b7c94d41b56abf91cbeb0c07626fa20d
Peter Carroll <petelc1966@hotmail.com>
2025-01-18 06:55:33 -0500

    Delete WMS directory

--- WMS/Application/Requests/RequestParams.cs
+++
@@ -1,9 +0,0 @@
-using Application.Core;
-
-namespace Application.Requests
-{
-    public class RequestParams : PagingParams
-    {
-        public bool IsNew { get; set; }
-    }
-}
--- WMS/client/src/pages/request/RequestPage.tsx
+++
@@ -1,5 +0,0 @@
-import RequestSide from '../../components/requests/request_form/RequestSide';
-
-export default function RequestPage() {
-  return <RequestSide />;
-}
